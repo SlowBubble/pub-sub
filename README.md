f98b30dd2f463c21c54e5c48e9ea4d26ced4c7eb
Slow Bubble <club.fest.on.meteor@gmail.com>
2020-08-19 17:00:53 -0400

    add module type

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 
 
-Usage:
+# Usage
 
 ```
-const [noteOnPub, noteOnSub] = pubsub.make();
+import * as pubSub from './node_modules/@clubfest/pub-sub/pubSub.js'
+
+const [noteOnPub, noteOnSub] = pubSub.make();
 noteOnSub(data => { console.log('Received: ', data); });
 noteOnPub(42);  // log: 'Recieved: 42'
 ```
--- pubSub.js
+++ pubSub.js
@@ -1,8 +1,10 @@
 /**
  * Usage:
-    const [noteOnPub, noteOnSub] = pubsub.make();
+    import * as pubSub from './node_modules/@clubfest/pub-sub/pubSub.js'
+
+    const [noteOnPub, noteOnSub] = pubSub.make();
     noteOnSub(data => { console.log('Received: ', data); });
-    noteOnPub(42);
+    noteOnPub(42);  // log: 'Recieved: 42'
  *   
  *   
  */
